eb9b08ebccf434a713cca4f20672c2f2b9cf50b3
consuti-tec <consuti@consuti.com.br>
2024-09-08 20:59:38 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#000000",
+  UrlPortal: "https://escoladoesporte.com.br/EB",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
